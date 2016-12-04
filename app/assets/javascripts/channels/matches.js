9870bae472859cfcd109d992634c53808d4e6a89
Chenje Katanda <chenje@ssn.co.zw>
2016-12-04 04:17:22 -0500

    Get action cable to work for live scores for each match

--- app/assets/javascripts/channels/matches.js
+++ app/assets/javascripts/channels/matches.js
@@ -1,6 +1,20 @@
 App.matches = App.cable.subscriptions.create("MatchesChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
+    // var matchId;
+    if (matchId = $('.match-body').data('match-id')) {
+      return this.perform("follow", { match_id: matchId})
+    } else {
+        return this.perform("nofollow");
+    };
+
+    // if (matchId = $('.match-body').data('match-id')) {
+    //     return this.perform("follow");
+    // } else {
+    //     return this.perform("nofollow");
+    // };
+
+
   },
 
   disconnected: function() {
@@ -9,7 +23,8 @@ App.matches = App.cable.subscriptions.create("MatchesChannel", {
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
-    alert(data['message']);
+    $(".score1").html(data['score1'])
+    $(".score2").html(data['score2'])
   },
 
   update: function() {
--- app/channels/matches_channel.rb
+++ app/channels/matches_channel.rb
@@ -1,14 +1,16 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class MatchesChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "matches"
+
+  def follow(data)
+    stop_all_streams
+    stream_from "match_#{data['match_id']}"
   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+  def unfollow
+    stop_all_streams
   end
 
-  def update
-      ActionCable.server.broadcast "matches", message: "It works"
-  end
+  # def update
+  #     ActionCable.server.broadcast "matches", message: "It works"
+  # end
 end
