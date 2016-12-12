2c216b0ce4ac5c5a7763ef3eb2defc30e5ebf173
Chenje Katanda <chenje@ssn.co.zw>
2016-12-11 20:40:55 -0500

    pretty URLs with custom to_param

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,7 @@
 class Match < ApplicationRecord
     belongs_to :scorer
     has_many :match_events
+    def to_param
+        "#{id}-#{teamAName}-vs-#{teamBName}-#{matchdate.strftime("%A %e %B %Y")}".parameterize
+    end
 end
