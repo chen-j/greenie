8f491790eac908042c8d5349978aa275a3346dc0
Chenje Katanda <chenje@ssn.co.zw>
2016-12-14 00:28:46 +0200

    Remove custom to_param

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,5 @@
 class Match < ApplicationRecord
     belongs_to :scorer
     has_many :match_events
-    def to_param
-        "#{id}-#{teamAName}-vs-#{teamBName}-#{matchdate.strftime("%A %e %B %Y")}".parameterize
-    end
+
 end
