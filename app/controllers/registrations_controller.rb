120926953ed55027b6972785b55e2c2c20fedf09
Chenje Katanda <chenje@ssn.co.zw>
2017-05-18 00:02:13 +0200

    Fix bootstrap grid/disable clock and events as well as updated admin tracking screen

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
     protected
 
     def after_sign_up_path_for(resource)
-      '/subscribers/new'
+      '/my_matches'
     end
 
 end
