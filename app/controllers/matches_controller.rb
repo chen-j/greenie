e52c92782a184cb2cf3b598c4b34590fa7059632
Chenje Katanda <chenje@ssn.co.zw>
2017-05-19 10:18:00 +0200

    Fix no method error for date

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,7 +1,7 @@
 class MatchesController < ApplicationController
 
     def index
-        @upcoming_matches = Match.where("matchdate >= ?", DateTime.now.to_date..DateTime.end_of_week)
+        @upcoming_matches = Match.where("matchdate >= ?", DateTime.now.to_date..7.days.from_now)
         @matches = Match.order(matchdate: :asc)
         render "matches_home"
     end
